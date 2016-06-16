9f804b7f0644b60b522bb6eabba46845864bd7e3
menglin.chen <menglin.chen>
2016-06-16 10:04:38 +0800

    添加log拦截器

--- Rxtest/src/com/cml/rx/android/api/ApiClient.java
+++ Rxtest/src/com/cml/rx/android/api/ApiClient.java
@@ -10,6 +10,12 @@ import com.google.gson.Gson;
 import com.squareup.okhttp.Interceptor;
 import com.squareup.okhttp.Response;
 
+import okhttp3.OkHttpClient;
+import okhttp3.OkHttpClient.Builder;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
+import okhttp3.logging.HttpLoggingInterceptor.Logger;
+
 public class ApiClient {
 
 	private static final String APP_ID = "1ab6250f9a9a0441947ba6ddeee0d5ee";
@@ -42,21 +48,19 @@ public class ApiClient {
 	 * @return
 	 */
 	private Retrofit build(String baseUrl) {
-		Retrofit retrofit = new Retrofit.Builder()
-				.addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-				.addConverterFactory(GsonConverterFactory.create(new Gson()))
-				.baseUrl(baseUrl).build();
+		Retrofit retrofit = new Retrofit.Builder().addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+				.addConverterFactory(GsonConverterFactory.create(new Gson())).baseUrl(baseUrl).build();
+
+		retrofit.client().interceptors().add(new com.cml.rx.android.api.HttpLoggingInterceptor());
 		retrofit.client().interceptors().add(new Interceptor() {
 
 			@Override
 			public Response intercept(Chain chain) throws IOException {
-				com.squareup.okhttp.Request.Builder requestBuilder = chain
-						.request().newBuilder();
+				com.squareup.okhttp.Request.Builder requestBuilder = chain.request().newBuilder();
 				requestBuilder.addHeader("X-Bmob-Application-Id", APP_ID);
 				requestBuilder.addHeader("X-Bmob-REST-API-Key", API_KEY);
 				requestBuilder.addHeader("Content-Type", "application/json");
-				System.out.println("===========>"
-						+ requestBuilder.build().httpUrl());
+				System.out.println("===========>" + requestBuilder.build().httpUrl());
 				return chain.proceed(requestBuilder.build());
 			}
 		});
